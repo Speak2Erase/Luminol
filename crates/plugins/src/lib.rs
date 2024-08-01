91bd830b00d471bbb5398bd07edd193f7d129db7
Melody Madeline Lyons <lily@nowaffles.com>
2024-08-01 01:01:01 -0700

    stuff

--- crates/plugins/src/lib.rs
+++ crates/plugins/src/lib.rs
@@ -1,14 +1,41 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct Plugins {
+    pub engine: wasm_bridge::Engine,
+    pub loaded_plugins: slab::Slab<Plugin>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct Plugin {
+    instance: wasm_bridge::Instance,
+    store: wasm_bridge::Store<()>,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Default for Plugins {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Plugins {
+    pub fn new() -> Self {
+        let engine = wasm_bridge::Engine::default();
+        let loaded_plugins = slab::Slab::new();
+        Self {
+            engine,
+            loaded_plugins,
+        }
+    }
+
+    #[allow(deprecated)]
+    pub fn load_plugin(&mut self, plugin_bytes: &[u8]) -> wasm_bridge::Result<usize> {
+        let module = wasm_bridge::Module::new(&self.engine, plugin_bytes)?;
+        let mut linker = wasm_bridge::Linker::new(&self.engine);
+
+        let mut store = wasm_bridge::Store::new(&self.engine, ());
+
+        let instance = linker.instantiate(&mut store, &module)?;
+
+        let plugin = Plugin { instance, store };
+        let key = self.loaded_plugins.insert(plugin);
+
+        todo!();
     }
 }
