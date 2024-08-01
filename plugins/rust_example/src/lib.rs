cdbcd5a4c6abf50fbcb1c9973def0087c364c70d
Melody Madeline Lyons <lily@nowaffles.com>
2024-08-01 00:12:16 -0700

    Add basic plugin

---
+++ plugins/rust_example/src/lib.rs
@@ -0,0 +1,16 @@
+// wit_bindgen generates an unsafe extern "C" fn with no unsafe block inside it, so we NEED to allow this
+#![allow(unsafe_op_in_unsafe_fn)]
+
+wit_bindgen::generate!({
+    world: "test"
+});
+
+struct TheGuest;
+
+impl Guest for TheGuest {
+    fn run() {
+        print("Hello from Rust!");
+    }
+}
+
+export!(TheGuest);
